80febb024a1872ee746faa6beb2164b6e3fa3b2a
Pedro <pedrohenrique2604@gmail.com>
2018-06-24 18:12:01 -0300

    Criar página especifica para perfil.

---
+++ client/Perfil/Pefil.js
@@ -0,0 +1,20 @@
+/* gets the routs according to usersnames
+https://atmospherejs.com/kadira/flow-router
+*/
+Template.Perfil.helpers({
+	perfil: function() {
+// Reactive function which you can use to get a parameter from the URL
+		var idDoUsuario = FlowRouter.getParam("id");
+		console.log(idDoUsuario);
+//user's info
+		var info = Meteor.users.findOne({_id: idDoUsuario});
+		console.log(info);
+		return info;
+	},
+//finds authors' posts to put them in their profile
+	posts: function() {
+		var idDoUsuario = FlowRouter.getParam("id");
+		var postDoPerfil = Posts.find({idDoAutor: idDoUsuario}).fetch();
+		return postDoPerfil;
+	}
+});
